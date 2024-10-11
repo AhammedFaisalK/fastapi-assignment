028f433c264bbfd0030d80b279daf5b1a709d08e
Ahammed Faisal <ahammedfaisal@tegain.com>
2024-10-11 21:51:51 +0530

    database config

--- app/database.py
+++ app/database.py
@@ -1,1 +1,11 @@
- 
+from pymongo import MongoClient
+
+# Replace with your MongoDB connection string
+MONGO_CONNECTION_STRING = "<mongodb+srv://ahammedfaisal70:<db_password>@cluster0.fhi0n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0>"
+
+client = MongoClient(MONGO_CONNECTION_STRING)
+db = client["mydatabase"]
+
+# Collections
+items_collection = db["items"]
+clockin_collection = db["clock_in_records"]
