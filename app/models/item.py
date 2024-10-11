c30d0306e1022eb09e38224ac02c0458c0055533
Ahammed Faisal <ahammedfaisal@tegain.com>
2024-10-11 21:53:11 +0530

    Item model added

--- app/models/item.py
+++ app/models/item.py
@@ -1,1 +1,15 @@
- 
+from pydantic import BaseModel
+from typing import Optional
+
+class Item(BaseModel):
+    name: str
+    email: str
+    item_name: str
+    quantity: int
+    expiry_date: str
+
+class ItemUpdate(BaseModel):
+    name: Optional[str]
+    item_name: Optional[str]
+    quantity: Optional[int]
+    expiry_date: Optional[str]
