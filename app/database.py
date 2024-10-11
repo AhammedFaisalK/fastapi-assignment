c875311d2f6be44a79122890ce1faa7e000496f4
Ahammed Faisal <ahammedfaisal@tegain.com>
2024-10-11 22:28:22 +0530

    Updated env and database.py

--- app/database.py
+++ app/database.py
@@ -1,7 +1,11 @@
+import os
 from pymongo import MongoClient
+from dotenv import load_dotenv
 
-# Replace with your MongoDB connection string
-MONGO_CONNECTION_STRING = "<mongodb+srv://ahammedfaisal70:<db_password>@cluster0.fhi0n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0>"
+# Load environment variables from .env file
+load_dotenv()
+
+MONGO_CONNECTION_STRING = os.getenv("MONGO_CONNECTION_STRING")
 
 client = MongoClient(MONGO_CONNECTION_STRING)
 db = client["mydatabase"]
